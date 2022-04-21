a8086d93206244ccd23e94b32023a40648f46b25
Guillaume P <guillaume@guillaumepasquet.fr>
2022-04-21 19:05:08 -0400

    work

--- src/three/shaders/fragmentShader.glsl
+++ src/three/shaders/fragmentShader.glsl
@@ -1,6 +1,8 @@
 precision mediump float;
 
+varying float vElevation;
+
 void main()
 {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1);
+    gl_FragColor = vec4(0.0, 0.0, 0.0, vElevation);
 }
--- src/three/world.ts
+++ src/three/world.ts
@@ -8,11 +8,32 @@ const clock = new THREE.Clock()
 export const world = new THREE.Group();
 
 // const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 50, 50, 50);
-// const geometry = new THREE.PlaneBufferGeometry(1, 1, 50, 50);
+// const geometry = new THREE.PlaneBufferGeometry(2, 2, 100, 100);
 
 const geometry = new THREE.BufferGeometry();
-const nbVertices = 100000;
-const verticesCoordinates = Array.from({ length: nbVertices * 3 }, () => Math.random() * 2 - 1);
+const nbVertices = 50;
+// const verticesCoordinates = Array.from({ length: nbVertices * 3 }, () => Math.random() * 2 - 1);
+const verticesCoordinates: number[] = [];
+const cubeSize = 2;
+Array.from({ length: nbVertices }).forEach((noop, x) => {
+  Array.from({ length: nbVertices }).forEach((noop, y) => {
+    Array.from({ length: nbVertices }).forEach((noop, z) => {
+      const point = getPoint();
+      verticesCoordinates.push(
+        point.x,
+        point.y,
+        point.z,
+        // Math.cos(x * 2 * Math.PI / nbVertices) * y / nbVertices, 
+        // Math.sin(x * 2 * Math.PI / nbVertices) * y / nbVertices, 
+        // Math.tan(x * 2 * Math.PI / nbVertices)
+
+        // x * cubeSize / nbVertices - cubeSize / 2 + Math.random() * cubeSize / nbVertices / 10, 
+        // y * cubeSize / nbVertices - cubeSize / 2 + Math.random() * cubeSize / nbVertices / 10, 
+        // z * cubeSize / nbVertices - cubeSize / 2 + Math.random() * cubeSize / nbVertices / 10
+      );
+    });
+  });
+});
 const vertices = new Float32Array(verticesCoordinates);
 geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
 
@@ -40,3 +61,22 @@ export const tick = () => {
 world.add(points);
 
 
+
+
+
+
+function getPoint() {
+  var u = Math.random();
+  var v = Math.random();
+  var theta = u * 2.0 * Math.PI;
+  var phi = Math.acos(2.0 * v - 1.0);
+  var r = Math.cbrt(Math.random());
+  var sinTheta = Math.sin(theta);
+  var cosTheta = Math.cos(theta);
+  var sinPhi = Math.sin(phi);
+  var cosPhi = Math.cos(phi);
+  var x = r * sinPhi * cosTheta;
+  var y = r * sinPhi * sinTheta;
+  var z = r * cosPhi;
+  return {x: x, y: y, z: z};
+}
